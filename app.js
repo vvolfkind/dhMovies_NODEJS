4b730c244927a7abe42d8ba5ee7f025e7316c930
Rodo <rodolfo.raskovsky85@gmail.com>
2018-12-12 15:57:11 -0300

    refactor app.js

--- app.js
+++ app.js
@@ -7,25 +7,12 @@ const app = express();
 
 const router = require('./routes.js');
 
-// models.sequelize.sync()
-// .then(function () {
-//   console.log('conectado a la DB')
-// })
-// .catch(function (err) {
-//   console.log(err)
-// });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.use('/', router);
-
-// app.get('/', function (req, res) {
-//   console.log('app listening on port: ' + port);
-//   res.send('test movies express nodejs mysql')
-// });
 
 app.listen(port, () => {
   console.log(`server levantado en puerto ${port}`);
